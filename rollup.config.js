a3eaac4d004e4774ef7a61bc653222a069278898
Chris Milson <chris.w.milson@gmail.com>
2020-02-28 12:11:39 +0900

    Add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript2'
+
+const tsconfigDefaults = {
+  compilerOptions: {
+    declaration: true,
+    allowSyntheticDefaultImports: true
+  },
+  exclude: [
+    'node_modules',
+    '**/__tests__/*'
+  ]
+}
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'lib',
+    format: 'cjs'
+  },
+  plugins: [typescript({ tsconfigDefaults })]
+}
