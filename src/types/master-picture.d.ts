20f959236ec1e05673f4dbfca8b2eb18dc0b90ba
Chris Milson <chris.w.milson@gmail.com>
2020-02-28 10:13:53 +0900

    Add path master picture
    The path master picture can be translated into the inlinemaster picture,
    and this extra sub type defines how that should be formed.

--- src/types/master-picture.d.ts
+++ src/types/master-picture.d.ts
@@ -12,6 +12,18 @@ interface InlineMasterPicture {
   content: string
 }
 
+/**
+ * This type of master picture cannot be sent to the REST Api.
+ * It CAN however be converted into something suitable with the createRequest
+ * method.
+ */
+export interface PathMasterPicture {
+  /** There is a master picture to transmit, located in the local filesystem. */
+  type: 'path'
+  /** The path to the master picture */
+  path: string
+}
+
 type MasterPicture = URLMasterPicture | InlineMasterPicture
 
 export default MasterPicture
