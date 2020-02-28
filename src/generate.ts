b12aead9db281b9db8b4177d8ce6cc7cb9bf1623
Chris Milson <chris.w.milson@gmail.com>
2020-02-28 09:41:52 +0900

    Unzip package
    We could get the request response and now we can download the generated
    icons and extract them to a desired directory.

--- src/generate.ts
+++ src/generate.ts
@@ -1,19 +1,38 @@
 import axios from 'axios'
-import { FaviconGenerationManifest } from './types'
+import unzipper from 'unzipper'
+import fs from 'fs'
+import path from 'path'
+import {
+  FaviconGenerationManifest,
+  FaviconGenerationResult
+} from './types'
 
 /**
+ * When supplied with a valid request object, will use the REST API to generate
+ * the desired icons and write them to a destination folder.
+ * 
  * @param request The favicon generation manifest
  * @param dest 
  */
 export default async function generateFavicon(
   request: FaviconGenerationManifest,
   dest: string
-) {
+): Promise<FaviconGenerationResult> {
   // send a post request to the rest api to generate the favicon.
+  // The response should give us the status of our request, and a URL to
+  // download a zip of the assets from.
   const { data } = await axios.post(
     'https://realfavicongenerator.net/api/favicon',
     { favicon_generation: request }
   )
   
-  return data
+  // assume success
+  const { package_url: zipURL } = data.favicon_generation_result.favicon
+
+  // download the package and extract it to the destination
+  await axios.get(zipURL, {
+    responseType: 'stream'
+  }).then(res => res.data.pipe(unzipper.Extract({ path: dest })))
+
+  return data.favicon_generation_result
 }
