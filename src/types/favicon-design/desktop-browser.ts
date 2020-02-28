d1a72a7ed84204a6a876e199312cb06e442eecb0
Chris Milson <chris.w.milson@gmail.com>
2020-02-28 13:36:55 +0900

    Allow empty interface
    The rest api expects either undefined or an empty object for the desktop
    browser design config, so the documentation should reflect that.

--- src/types/favicon-design/desktop-browser.ts
+++ src/types/favicon-design/desktop-browser.ts
@@ -3,4 +3,5 @@
  * philosophy behind is that the master picture is usually designed for this
  * purpose.
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export default interface DesktopBrowserFaviconDesign {}
