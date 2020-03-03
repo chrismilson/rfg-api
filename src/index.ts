3104ab84a566bafe13346076f7d00344608187c5
Chris Milson <chris.w.milson@gmail.com>
2020-03-03 11:28:06 +0900

    export generate favicon from module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { fileToBase64, fileToBase64Sync } from './file-to-base-64'
+import { generateFavicon } from './generate-favicon'
 
-export { fileToBase64, fileToBase64Sync }
+export { fileToBase64, fileToBase64Sync, generateFavicon }
 
 export function init() {
   return {
     fileToBase64,
-    fileToBase64Sync
+    fileToBase64Sync,
+    generateFavicon
   }
 }
