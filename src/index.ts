6c99da0adeea42c26e98f76e46e78b7d9c8b0b02
Chris Milson <chris.w.milson@gmail.com>
2020-03-03 13:29:23 +0900

    Export inject markups

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { fileToBase64, fileToBase64Sync } from './file-to-base-64'
 import { generateFavicon } from './generate-favicon'
+import { injectFaviconMarkups } from './inject-favicon-markups'
 
-export { fileToBase64, fileToBase64Sync, generateFavicon }
+export { fileToBase64, fileToBase64Sync, generateFavicon, injectFaviconMarkups }
 
 export function init() {
   return {
     fileToBase64,
     fileToBase64Sync,
-    generateFavicon
+    generateFavicon,
+    injectFaviconMarkups
   }
 }
