ecd2a862260e3903389c8474b83373015ca1d80e
Chris Milson <chris.w.milson@gmail.com>
2020-03-03 14:48:48 +0900

    Export camel case to underscore

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { fileToBase64, fileToBase64Sync } from './file-to-base-64'
 import { generateFavicon } from './generate-favicon'
 import { injectFaviconMarkups } from './inject-favicon-markups'
+import {
+  camelCaseToUnderscore,
+  camelCaseToUnderscoreRequest
+} from './camel-to-underscore'
 
-export { fileToBase64, fileToBase64Sync, generateFavicon, injectFaviconMarkups }
+export {
+  fileToBase64,
+  fileToBase64Sync,
+  generateFavicon,
+  injectFaviconMarkups,
+  camelCaseToUnderscore,
+  camelCaseToUnderscoreRequest
+}
 
 export function init() {
   return {
     fileToBase64,
     fileToBase64Sync,
     generateFavicon,
-    injectFaviconMarkups
+    injectFaviconMarkups,
+    camelCaseToUnderscore,
+    camelCaseToUnderscoreRequest
   }
 }
