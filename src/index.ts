97b06852a624e42aa1949d17ef6543851e7b03fa
Chris Milson <chris.w.milson@gmail.com>
2020-03-03 10:43:28 +0900

    Export with backward compatibility
    The old library had a single init export that when called would define
    the actual inside of the library.
    
    To keep backwards compatibility, we are keeping this functonality, and
    also exporting in the normal fashion.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export {
+import {
   fileToBase64,
   fileToBase64Sync
 } from './file-to-base-64'
+
+export {
+  fileToBase64,
+  fileToBase64Sync
+}
+
+export function init() {
+  return {
+    fileToBase64,
+    fileToBase64Sync
+  }
+}
